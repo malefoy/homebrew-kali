ec4770c139805fa37392c8504dd796bb39f6164e
malefoy <59148544+malefoy@users.noreply.github.com>
2020-02-09 13:42:14 +0100

    Update uniscan.rb

--- uniscan.rb
+++ uniscan.rb
@@ -4,7 +4,7 @@
 
 class Uniscan < Formula
   homepage ""
-  head "git@gitlab.com:kalilinux/packages/uniscan.git"
+  head "https://gitlab.com/kalilinux/packages/uniscan.git"
   
 
   # depends_on "cmake" => :build
